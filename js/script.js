85217fdef4b78274f414acb1917799e117a7845f
Rouabhia2002 <Rouabhiaamira21@gmail.com>
2024-04-02 02:20:33 +0200

    add the js part

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+const hum = document.querySelector('.header__hamburger');
+const headerMenu = document.querySelector('.header__menu');
+const header = document.querySelector('.header');
+const heroo = document.querySelector('.hero');
+const menu = document.getElementById('menu'); 
+
+menu.style.display = 'none'; 
+
+hum.addEventListener('click', function() {
+  if (header.classList.contains('open')) { 
+    document.body.classList.remove('no-scrol');
+    header.classList.remove('open');
+    heroo.classList.remove('overlay');
+    menu.style.display = 'none'; 
+  } else {
+    document.body.classList.add('no-scrol');
+    header.classList.add('open');
+    heroo.classList.add('overlay');
+    menu.style.display = 'block'; 
+  }
+});
+
